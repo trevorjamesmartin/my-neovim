c04d8d433425ae9b709d408a1a2c2c13fa0c22ad
trevorjamesmartin <trevor.j.martin@gmail.com>
2024-01-12 12:44:45 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # my-neovim
 neovim config
+
+based on [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
+
+a place to store my config whilst learning neovim...
+
+the original goal was to break up the kickstart config for easier consumption
+
+some additional plugins to help along the journey,
+- neo-tree, a file/project browser, opens with <C-\>
+- toggleterm, to access the terminal, opens with <C-`>
+- nightfly, a darker theme with higher contrast for my oldschool eyeballs
+
+
---
+++ init.lua
@@ -0,0 +1,34 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- load plugins
+require("my.plugins-setup")
+
+-- setup options 
+require("my.core.options")
+
+-- map keyboard shortcuts
+require("my.core.keymaps")
+
+-- configure plugins
+require("my.config.telescope")
+require("my.config.treesitter")
+require("my.config.lsp")
+require("my.config.nvim-cmp")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
