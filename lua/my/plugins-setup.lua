71be584adeb0a96bddb82818cd9340f43367845b
trevorjamesmartin <trevor.j.martin@gmail.com>
2024-01-12 21:59:43 -0500

    disabled plugin to fix conflicting keymap reported by which-key

--- lua/my/plugins-setup.lua
+++ lua/my/plugins-setup.lua
@@ -202,7 +202,7 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  -- { 'numToStr/Comment.nvim', opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
